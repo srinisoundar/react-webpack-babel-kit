05aefa6876ac6d96e1a73566b0b2af90a6261f3f
Srinivasan Soundararajan <srinivasan@backbase.com>
2016-11-28 15:18:05 +0530

    feat: update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -21,11 +21,6 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /(node_modules|bower_components)/,
-                loader: 'react-hot'
-            },
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
                 loader: 'babel',
                 query: {
                     presets: ['react', 'es2015']

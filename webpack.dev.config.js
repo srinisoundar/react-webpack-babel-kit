432651f17850843bf6cdd0480b2c8f7539ac6a8f
Srinivasan Soundararajan <ssound@sapient.com>
2017-06-06 22:14:24 +0530

    feat: add different flavors

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -5,13 +5,37 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
     context: resolve(__dirname, 'src'),
     entry: [
+        'react-hot-loader/patch',
+        // activate HMR for React
+
+        'webpack-dev-server/client?http://localhost:8080',
+        // bundle the client for webpack-dev-server
+        // and connect to the provided endpoint
+
+        'webpack/hot/only-dev-server',
+        // bundle the client for hot reloading
+        // only- means to only hot reload for successful updates
+
         './scripts/main.js'
         // the entry point of our app
     ],
     output: {
         filename: 'main.js',
+        path: resolve(__dirname, 'dist'),
+        publicPath: '/'
+        // necessary for HMR to know where to load the hot update chunks
     },
     devtool: 'source-map',
+    devServer: {
+        hot: true,
+        // enable HMR on the server
+
+        contentBase: resolve(__dirname),
+        // match the output path
+
+        publicPath: '/'
+        // match the output `publicPath`
+    },
     module: {
         rules: [
             {
@@ -41,6 +65,9 @@ module.exports = {
         ]
     },
     plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        // enable HMR globally
+
         new webpack.NamedModulesPlugin(),
         // prints more readable module names in the browser console on HMR updates
 
